def6dfc53ff21e3e8c322b9c7dc124a5775b2001
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-04-05 04:48:41 +0800

    first commit

---
+++ screens/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, SafeArea, FlatList, Text } from "react-native";
+import { COLORS, NFTData } from "../constants";
+
+const Home = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default Home;
