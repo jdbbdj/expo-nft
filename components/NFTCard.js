029165c4f5dd642b5f2b20f44e8aee6f0a6f9273
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-04-05 18:03:36 +0800

    FINISHED NFTCARD

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,6 +1,15 @@
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+import {
+  SubInfo,
+  ImageCmp,
+  People,
+  EndDate,
+  NFTTitle,
+  ETHPrice,
+} from "./SubInfo";
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
   return (
@@ -23,6 +32,31 @@ const NFTCard = ({ data }) => {
             borderTopLeftRadius: SIZES.font,
             borderTopRightRadius: SIZES.font,
           }}
+        />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
+      </View>
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+      </View>
+      <View
+        style={{
+          marginTop: SIZES.font,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <ETHPrice price={data.price} />
+        <RectButton
+          minWidth={120}
+          fontSize={SIZES.font}
+          handlePress={() => navigation.navigate("Details", { data })}
         />
       </View>
     </View>
